192b03608ab96e38c59e9f675eaac26d923586d1
Jaan Van Gils <j.j.g.vangils@students.uu.nl>
2023-07-07 11:05:16 +0200

    Include TemplateHaskell and unsafeCoerce to fix compatibility with lts-18.0

--- accelerate-io-vector/src/Data/Array/Accelerate/IO/Data/Primitive/ByteArray.hs
+++ accelerate-io-vector/src/Data/Array/Accelerate/IO/Data/Primitive/ByteArray.hs
@@ -19,6 +19,7 @@ import Data.Primitive.ByteArray
 
 import GHC.Base
 import GHC.ForeignPtr
+import GHC.Exts (unsafeCoerce#)
 
 
 -- Return the ByteArray underlying a ForeignPtr, or a new byte array if
